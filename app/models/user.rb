1bdcf1abb94b5761934e12dd1dab37516f7e70e2
Jacob Gillespie <mail@jacobwg.com>
2012-11-28 13:05:55 -0600

    Helper for resetting user message quotas

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,12 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.reset_quotas!
+    User.find_each do |u|
+      u.refill_quota! if u.sent?
+    end
+  end
+
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
