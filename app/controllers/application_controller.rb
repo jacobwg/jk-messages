0df246be310040ae74a443b6e3badcd22f606aef
Jacob Gillespie <mail@jacobwg.com>
2013-01-26 14:21:30 -0600

    Switching HTTP auth from Nginx to Rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
 
   before_filter :load_users
 
+  before_filter :authenticate
+
   def load_users
     @jacob = User.where(uid: Settings.jacob_id).first
     @kathryn = User.where(uid: Settings.kathryn_id).first
+  end
+
+  private
+
+  def authenticate
+    return if params[:auth] == Settings.auth_key
+    authenticate_or_request_with_http_digest(Settings.auth_realm) do |username|
+      Settings.auth_users[username]
+    end
   end
 end
