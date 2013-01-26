6637e6a53f8154af11fbe8fa90e373f65ee158d5
Jacob Gillespie <mail@jacobwg.com>
2013-01-26 12:14:00 -0600

    Added show action for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,24 @@ class MessagesController < ApplicationController
     respond_with @messages
   end
 
+  def show
+    @last_day = Message.last.time_cst.to_date
+    @day = !params[:day].nil? ? Date.parse(params[:day]) : @last_day
+
+    @first_day = Date.parse('2012-08-13')
+
+    all_days = (@first_day..@last_day).to_a
+    sent_days = Message.all.map { |m| m.time_cst.to_date }
+    @unsent_days = all_days - sent_days
+
+    @message = Message.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.pdf { render :text => PDFKit.new( message_url(@message) ).to_pdf }
+    end
+  end
+
   def search
     if params[:q]
       @last_day = Message.last.time_cst.to_date
