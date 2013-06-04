e5d4970db87a4aa3e8f5c17e68df671e91187469
Jacob Gillespie <mail@jacobwg.com>
2013-06-04 15:50:33 -0500

    Cleaned up and documented import.js

--- import.js
+++ import.js
@@ -1,13 +1,24 @@
+// import.js
+
+// Modules
 var fb = require('fb'),
     Firebase = require('firebase'),
+    moment = require('moment'),
     nconf = require('nconf'),
-    when = require('when'),
-    moment = require('moment');
+    when = require('when');
 
+// Load configuration
 nconf.argv()
-   .env()
-   .file({ file: 'config.json' });
+     .env()
+     .file({ file: 'config.json' });
 
+// Set Facebook access token
+fb.setAccessToken(nconf.get('ACCESS_TOKEN'));
+
+// URL pattern for autolinking
+var url_pattern = /(^|\s)(\b(https?|ftp):\/\/[\-A-Z0-9+\u0026@#\/%?=~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~_|])/gi;
+
+// Convenience method for performing FQL queries
 var queryFB = function(query) {
   var deferred = when.defer();
 
@@ -22,34 +33,40 @@ var queryFB = function(query) {
   return deferred.promise;
 };
 
+// Connect and authorize Firebase
 var db = new Firebase('https://jacob-and-kathryn.firebaseio.com');
 db.auth(nconf.get('FIREBASE_SECRET'));
-
 var dataDB = db.child('data');
 var messagesDB = db.child('messages');
 
-var url_pattern = /(^|\s)(\b(https?|ftp):\/\/[\-A-Z0-9+\u0026@#\/%?=~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~_|])/gi;
-
+// Method to load message by ID
 var loadMessage = function(id) {
+  // Set up promise
   var deferred = when.defer();
 
+  // Build query
   var q = 'SELECT attachment, author_id, body, created_time, message_id, source, thread_id FROM message WHERE message_id="510521608973600_' + id + '"';
+
+  // Make query
   queryFB(q).then(function(data) {
     var message = data.data[0];
 
+    // Calculate data
     message.word_count = ((message.body || ' ').match(/\S+/g) || []).length;
     message.author_key = (message.author_id == '100000505263000') ? 'jacob' : 'kathryn';
     message.name = (message.author_id == '100000505263000') ? 'Jacob Gillespie' : 'Kathryn Elizabeth';
     message.header = moment.unix(message.created_time).format("dddd, MMMM Do YYYY, h:mm:ss a") + ' - ' + message.name + ':';
-
     message.local_id = parseInt(message.message_id.replace('510521608973600_', ''), 10);
 
+    // Build HTML display
     message.html = message.body.replace(/\n{2,}/g, '</p><p>').replace(/\n/g, '<br/>');
     message.html = '<p>' + message.html + '</p>';
     message.html = message.html.replace(url_pattern, "$1<a href='$2'>$2</a>");
 
-    console.log('Fetched message ID ' + id);
+    // Save message with priority
     messagesDB.child(id).setWithPriority(message, message.created_time);
+
+    // Increment the current message ID
     dataDB.child('currentMessage').transaction(function(currentMessage) {
       if (currentMessage < id)
         return id;
@@ -57,6 +74,7 @@ var loadMessage = function(id) {
         return currentMessage;
     });
 
+    // Set the last message send time
     dataDB.child('lastMessageTime').transaction(function(lastMessageTime) {
       if (lastMessageTime < message.created_time)
         return message.created_time;
@@ -64,33 +82,33 @@ var loadMessage = function(id) {
         return lastMessageTime;
     });
 
+    // Increment word count
     dataDB.child('wordCount').transaction(function(wordCount) {
       return wordCount + message.word_count;
     });
 
+    // Resolve the promise
     deferred.resolve(message);
+
   }, function(err) {
+
+    // Reject the promise
     deferred.reject(err);
+
   });
 
+  // Return the promise
   return deferred.promise;
 };
 
-var checkMessage = function(id) {
-  var deferred = when.defer();
-
-  messagesDB.child(id).once('value', function(snap) {
-    if (!snap.val()) {
-      console.log('Fetching message ID ' + id);
-      deferred.resolve(loadMessage(id));
-    }
-  });
-
-  return deferred.promise;
+// Function for the success message
+var successMessage = function(message) {
+  console.log('Fetched message ID ' + message.local_id);
 };
 
+// Method to check for new messages
 var checkForMessages = function(data) {
-  console.log('Checking for messages...');
+  console.log('\nChecking for messages...');
   dataDB.once('value', function(snap) {
     var data = snap.val();
     queryFB('SELECT message_count FROM thread WHERE thread_id=' + nconf.get('THREAD_ID')).then(function(fb) {
@@ -100,21 +118,17 @@ var checkForMessages = function(data) {
 
       if (lastMessage > data.currentMessage) {
         for (var i = data.currentMessage + 1; i <= lastMessage; i++) {
-          checkMessage(i);
+          loadMessage(i).then(successMessage);
         }
       } else {
-        console.log('All messages imported...');
+        console.log('Messages already imported.');
       }
     });
   });
 };
 
-fb.setAccessToken(nconf.get('ACCESS_TOKEN'));
-
-// check for messages
+// Check for messages on first run
 checkForMessages();
 
-// check for messages every minute
+// Check for messages afterwards every minute
 setInterval(checkForMessages, 60 * 1000);
-
-
